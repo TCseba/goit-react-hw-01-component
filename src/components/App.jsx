2592b7c4c759d7fb33fe95b4688e227e3eced2e0
TCseba <t.seba143@gmail.com>
2025-03-22 20:45:57 +0200

    adding data folder

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,17 +10,17 @@ import transactions from '../data/transactions.json';
 
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template -- my first attempt
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
